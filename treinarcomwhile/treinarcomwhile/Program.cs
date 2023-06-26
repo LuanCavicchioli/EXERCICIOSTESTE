888927b35ad88c2df6e35b8ac2b2d0477fb1a870
Luan Aziani <luan.caaziani@senacsp.edu.br>
2023-06-25 21:20:22 -0300

    Treino com comando while

---
+++ treinarcomwhile/treinarcomwhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treinarcomwhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tente adivinhar o numero digitado acima:");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            while (numero != numero1) 
+            {
+            Console.WriteLine("Digite um numero novamente");
+                numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite um numero novamente:");
+            }
+            Console.WriteLine("Digitou corretamente");
+  
+        }
+        
+    }
+}
