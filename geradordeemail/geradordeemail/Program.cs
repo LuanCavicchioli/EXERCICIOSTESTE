73d6b9fdcc93b1ef74b3758b711850f0f49fc695
Luan Aziani <luan.caaziani@senacsp.edu.br>
2023-06-25 21:15:54 -0300

    Movido

---
+++ geradordeemail/geradordeemail/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geradordeemail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = {"Luan","thiago","jao","noia","pexe"};
+            string[] numero = { "1" + "2" + "3" + "4" + "5" + "6" + "7" + "8" + "9" + "10" };
+            string[] provedores = {"gmail.com","hotmail.com", "outlook.com"};
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.WriteLine("Voce deseja utilizar qual dominio, 1 para gmail, 2 para hotmail, 3 para outlook");
+                string opçao = Console.ReadLine();
+
+                string nome = nomes[random.Next(nomes.Length)];
+                string numeros = numero[random.Next(numero.Length)];
+                string provedor = string.Empty;
+
+                switch (opçao)
+                {
+                case "1":
+                        provedor = provedores[0];
+
+
+                        break;
+
+                case "2":
+
+                        provedor = provedores[1];
+
+                        break;
+
+
+                case "3":
+                        provedor = provedores[2];
+
+                        break;
+
+                case "4":
+                        string provedor4 = provedores[random.Next(numero.Length)];
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida ou nao correspondente ");
+                        continue;
+                }
+
+                string email = nome +"."+ numeros + "@" +provedor;
+                Console.WriteLine("Seu email gerado é :" + email);
+                break;  
+                
+                
+            }
+
+        }
+    }
+}
---
+++ geradordesenhasaleatoria/geradordesenhasaleatoria/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geradordesenhasaleatoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = string.Empty;
+            int tamanhoSenha = 0;
+            string caractersSenha = string.Empty;
+            Random random = new Random();
+
+           
+
+            while (true) {
+                Console.WriteLine("Digite a senha de 1 a 10 ;");
+                string opção = Console.ReadLine();
+                switch (opção)
+                {
+                    case "1":
+                        tamanhoSenha = 1;
+
+                        caractersSenha = "ABCD1234@##!";
+
+                        break;
+
+
+                    case "2":
+                        tamanhoSenha = 2;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                        break;
+
+
+
+                    case "3":
+                        tamanhoSenha = 3;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    case "4":
+                        tamanhoSenha = 5;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    case "5":
+                        tamanhoSenha = 5;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    case "6":
+                        tamanhoSenha = 6;
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+                    case "7":
+                        tamanhoSenha = 7;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    case "8":
+                        tamanhoSenha = 8;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    case "9":
+                        tamanhoSenha = 9;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    case "10":
+                        tamanhoSenha = 10;
+
+                        caractersSenha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789100";
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção invalida ou nao disponivel");
+                        continue;
+
+                }
+                break;
+                }
+           
+            for (int i = 0; i < tamanhoSenha; i++)
+            {
+                int index = random.Next(caractersSenha.Length);
+                senha += caractersSenha[index];
+            }
+            Console.WriteLine("Senha gerada: " + senha);
+
+        }
+        
+            
+    }
+}
